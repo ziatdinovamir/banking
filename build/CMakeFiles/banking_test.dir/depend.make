db6c9d5c377bb358ba44c4c3868a34561183f2bd
ziatdinovamir <ziatdinov.amir.07@gmail.com>
2026-03-31 16:19:36 +0300

    Remove CMakeCache.txt and add to gitignore

--- build/CMakeFiles/banking_test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for banking_test.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amir/ziatdinovamir/workspace/projects/banking
-# Build directory: /home/amir/ziatdinovamir/workspace/projects/banking/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(banking_test "/home/amir/ziatdinovamir/workspace/projects/banking/build/banking_test")
-set_tests_properties(banking_test PROPERTIES  _BACKTRACE_TRIPLES "/home/amir/ziatdinovamir/workspace/projects/banking/CMakeLists.txt;45;add_test;/home/amir/ziatdinovamir/workspace/projects/banking/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
